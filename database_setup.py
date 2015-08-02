9e89387770d9e0a76f7b1eba7178bdc7e332f651
Samuel Gaspar <s.gaspar.85@gmail.com>
2015-08-02 22:29:53 +0200

    feat: Add Cascade Delete

--- database_setup.py
+++ database_setup.py
@@ -14,6 +14,7 @@ class Categories(Base):
 
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    items = relationship('Items', cascade="delete")
 
 
 # Items table definition
